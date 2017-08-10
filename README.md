ffe3cd7f5fdd5c4c350cd64f3bad20833c4c8653
Jineon Kim <cos@getsliejoy.com>
2017-08-10 19:53:54 +0900

    sample app

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# sample-buzzad-banner
-Sample App for BuzzAD Banner
+Prerequisite: Adding repositories and dependencies to build.gradle
+
+repositories {
+    maven { url "https://dl.bintray.com/buzzvil/buzzscreen/" }
+    maven { url "http://dl.appnext.com/" }
+}
+...
+dependencies {
+    compile("com.buzzvil.buzzad:buzzad-sdk:")
+    compile('com.android.support:multidex:1.0.1')
+}
+
+Step 1: Adding Buzzad adView
+In your layout file (for example: /res/layout/activity_main.xml), add Buzzad adView.
+
+<?xml version="1.0" encoding="utf-8"?>
+<RelativeLayout
+ ...
+ >
+   ...
+    <com.buzzvil.buzzad.sdk.banner.AdView
+        android:id="@+id/adView"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content">
+    </com.buzzvil.buzzad.sdk.banner.AdView>
+   ...
+</RelativeLayout>
+
+
+Step 2: Loading an Ad
+
+private AdView adView;
+
+@Override
+public void onCreate(Bundle savedInstanceState) {
+ 
+ ...
+     // Find the instantiated AdView view
+     adView = (AdView) findViewById(R.id.adView);
+
+     // Add an Ad Listener
+    adView.setAdListener(new AdListener() {
+        @Override
+        public void onError(BuzzAdError error) {
+        }
+
+        @Override
+        public void onAdLoaded(Ad ad) {
+        }
+
+        @Override
+        public void onClicked(Ad ad) {
+        }
+    });
+
+    // Request an ad
+    adView.loadAd(121658059380746);
+}
+
+Optional: Setting Demo
---
+++ app/src/main/java/com/buzzvil/banner/sample/SampleActivity.java
@@ -0,0 +1,75 @@
+package com.buzzvil.banner.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.buzzvil.buzzad.sdk.BuzzAdError;
+import com.buzzvil.buzzad.sdk.BuzzOptions;
+import com.buzzvil.buzzad.sdk.BuzzSDK;
+import com.buzzvil.buzzad.sdk.UserProfile;
+import com.buzzvil.buzzad.sdk.banner.Ad;
+import com.buzzvil.buzzad.sdk.banner.AdListener;
+import com.buzzvil.buzzad.sdk.banner.AdView;
+
+public class SampleActivity extends Activity {
+
+	private AdView adView;
+	private View buttonStart, buttonStop;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sample);
+
+		buttonStart = findViewById(R.id.buttonStart);
+		buttonStop = findViewById(R.id.buttonStop);
+
+		buttonStart.setOnClickListener(onClickButtonStart);
+		buttonStop.setOnClickListener(onClickButtonStop);
+	}
+
+	View.OnClickListener onClickButtonStart = new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			// Optional
+			BuzzSDK.setUserProfile(new UserProfile.Builder()
+					.setBirthday("1990-12-31")
+					.setGender(UserProfile.USER_GENDER_FEMALE)
+					.build());
+
+			// Optional, for Testing
+			// BuzzSDK.setOptions(new BuzzOptions.Builder().setDebugMode(1).build());
+
+			adView = (AdView) findViewById(R.id.adView);
+			adView.setAdListener(adListener);
+			adView.loadAd("121658059380746");
+		}
+	};
+
+	View.OnClickListener onClickButtonStop = new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			adView.destroy();
+			adView = null;
+		}
+	};
+
+	AdListener adListener = new AdListener() {
+		@Override
+		public void onError(BuzzAdError error) {
+			Toast.makeText(getApplicationContext(), "Ad Failed:" + error, Toast.LENGTH_SHORT).show();
+		}
+
+		@Override
+		public void onAdLoaded(Ad ad) {
+			Toast.makeText(getApplicationContext(), "Ad loaded", Toast.LENGTH_SHORT).show();
+		}
+
+		@Override
+		public void onClicked(Ad ad) {
+			Toast.makeText(getApplicationContext(), "Ad Clicked", Toast.LENGTH_SHORT).show();
+		}
+	};
+}
